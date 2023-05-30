d6de19699626cf1c2495e735df1ca9b561b77c70
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2023-05-30 18:08:13 +0530

    empty cart done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { FlexContent, Footer, Hero, Navbar, Sales, Stories } from './components'
+import { Cart, FlexContent, Footer, Hero, Navbar, Sales, Stories } from './components'
 import {heroapi, popularsales,topratesales,highlight,sneaker, story, footerAPI} from './data/data'
 import './App.css'
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Cart/>
       <main className='flex flex-col gap-16 relative'>
         <Hero heroapi={heroapi}/>
         <Sales endpoint={popularsales} ifExists/>
